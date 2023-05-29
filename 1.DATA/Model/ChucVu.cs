d4809fc1aa280032d141813877b5a70d9080e18d
manhman2806 <manhman2806@gmail.com>
2023-05-29 21:51:15 +0700

    Add project files.

---
+++ 1.DATA/Model/ChucVu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DATA.Model
+{
+    public class ChucVu
+    {
+        public ChucVu()
+        {
+            nhanViens = new HashSet<NhanVien>();
+        }
+        public Guid Id { get; set; }
+        public string? Ten { get; set; }
+        //public virtual ICollection<NhanVien> nhanViens { get; set; }
+    }
+}
