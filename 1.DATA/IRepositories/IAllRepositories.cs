02cb975c9f04c2805cc4a0fa56ee77841c997ea2
manhman2806 <manhman2806@gmail.com>
2023-06-05 20:52:29 +0700

    Repositories

---
+++ 1.DATA/IRepositories/IAllRepositories.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DATA.IRepositories
+{
+    public interface IAllRepositories<TEntity> where TEntity : class
+    {
+        DbSet<TEntity> Entities { get; set; } // DBset tổng
+        // Các phương thức Lấy dữ liệu
+        Task<TEntity> GetByIdAsync(Guid id); // Lấy 1
+        Task<IEnumerable<TEntity>> GetAllAsync(); // Lấy tất
+        // Các phương thức thêm
+        Task<TEntity> AddOneAsyn(TEntity entity); // thêm 1
+        Task<TEntity> AddManyAsyn(IEnumerable<TEntity> entity); // thêm một loạt
+        // Các phương thức xóa
+        Task<TEntity> DeleteOneAsyn(TEntity entity);  // Xóa 1
+        Task<TEntity> DeleteManyAsyn(IEnumerable<TEntity> entity); // Xóa 1 loạt
+        // Các phương thức sửa
+        Task<TEntity> UpdateOneAsyn(TEntity entity); // Sửa 1
+        Task<IEnumerable<TEntity>> UpdateManyAsyn(IEnumerable<TEntity> entity); // Sửa 1 loạt
+    }
+}
---
+++ 1.DATA/Repositories/AllRepositories.cs
@@ -0,0 +1,86 @@
+﻿using _1.DATA.DatabaseContext;
+using _1.DATA.IRepositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DATA.Repositories
+{
+    public class AllRepositories<TEntity> : IAllRepositories<TEntity> where TEntity : class
+    {
+        private CuahangDbContext _dbContext;
+        public DbSet<TEntity> Entities { get; set; }
+        public AllRepositories(CuahangDbContext cuahangDbContext)
+        {
+            this._dbContext = cuahangDbContext;
+            this.Entities = _dbContext.Set<TEntity>();
+        }
+        public async Task<TEntity> AddOneAsyn(TEntity entity)
+        {
+            await this.Entities.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<TEntity> AddManyAsyn(IEnumerable<TEntity> entity)
+        {
+            Collection<TEntity> result = new Collection<TEntity>();
+            foreach (var item in entity) // thêm từng cái 1
+            {
+                Collection<TEntity> collects = result;
+                collects.Add(await this.AddOneAsyn(item));
+            }
+            return (TEntity)(IEnumerable<TEntity>)result;
+        }
+
+        public async Task<TEntity> DeleteOneAsyn(TEntity entity)
+        {
+            await Task.FromResult<TEntity>(this.Entities.Remove(entity).Entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<TEntity> DeleteManyAsyn(IEnumerable<TEntity> entity)
+        {
+            Collection<TEntity> result = new Collection<TEntity>();
+            foreach (var item in entity) // thêm từng cái 1
+            {
+                Collection<TEntity> collects = result;
+                collects.Add(await this.DeleteOneAsyn(item));
+            }
+            return (TEntity)(IEnumerable<TEntity>)result;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await Entities.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(Guid id)
+        {
+            return await Entities.FindAsync(id);
+        }
+
+        public async Task<TEntity> UpdateOneAsyn(TEntity entity)
+        {
+            _dbContext.Update(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<TEntity>> UpdateManyAsyn(IEnumerable<TEntity> entity)
+        {
+            Collection<TEntity> result = new Collection<TEntity>();
+            foreach (var item in entity) // thêm từng cái 1
+            {
+                Collection<TEntity> collects = result;
+                collects.Add(await this.UpdateOneAsyn(item));
+            }
+            return (IEnumerable<TEntity>)result;
+        }
+    }
+}
