8aeb7285bd7849a9e9ad5ebd8e82a0e68e3954cc
Nguyễn Công Tiến <tiennc@gapit.com.vn>
2023-06-17 21:47:02 +0700

    create client view

---
+++ 4.ClientView/IServices/IAllServices.cs
@@ -0,0 +1,14 @@
+﻿using _1_API.ViewModel.NhanVien;
+using Data.ModelsClass;
+
+namespace CustomerViews.IServices
+{
+    public interface IAllServices
+    {
+        Task<T> GetById<T>(string url,Guid id);
+        Task<T> Add<T>(string url,T model);
+        Task<bool> Remove<T>(string urlGetById, string urlRemove, Guid id);
+        Task<T> Update<T>(string url, T model, Guid id);
+        Task<List<T>> GetAll<T>(string url);
+    }
+}
---
+++ 4.ClientView/Services/AllServices.cs
@@ -0,0 +1,67 @@
+﻿using _1_API.ViewModel.ChucVu;
+using Data.ModelsClass;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using CustomerViews.IServices;
+using System.Text;
+
+namespace CustomerViews.Services
+{
+    public class AllServices : IAllServices
+    {
+        public AllServices()
+        {
+        }
+
+        public async Task<T> Add<T>(string url, T model)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            var postTask = await client.PostAsJsonAsync<T>("Create", model);
+            return model;
+        }
+
+        public async Task<List<T>> GetAll<T>(string url)
+        {
+            List<T> models = new List<T>();
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+            string TResponse = await response.Content.ReadAsStringAsync();
+            models = JsonConvert.DeserializeObject<List<T>>(TResponse);
+            return models;
+        }
+
+        public async Task<T> GetById<T>(string url, Guid id)
+        {
+            
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url + id);
+            string TResponse = await response.Content.ReadAsStringAsync();
+            T model = JsonConvert.DeserializeObject<T>(TResponse);
+            return model;
+        }
+
+        public async Task<bool> Remove<T>(string urlGetById, string urlRemove, Guid id)
+        {
+            T model = await GetById<T>(urlGetById, id);
+            HttpClient client = new HttpClient();
+            var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+            var response = await client.GetAsync(urlRemove + id);
+            string result = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public async Task<T> Update<T>(string url, T model, Guid id)
+        {
+            HttpClient client = new HttpClient();
+            var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(url + id.ToString(), content);
+            string result = await response.Content.ReadAsStringAsync();
+            return model;
+        }
+    }
+}
