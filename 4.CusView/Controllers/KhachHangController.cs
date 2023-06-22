d0ced74debca2a1bc65da7149a549d5bbdaf686d
manhman2806 <manhman2806@gmail.com>
2023-06-22 20:56:56 +0700

    fix

--- 4.CusView/Controllers/KhachHangController.cs
+++ 4.CusView/Controllers/KhachHangController.cs
@@ -20,48 +20,6 @@ namespace _4.CusView.Controllers
         {
             return View();
         }
-        //public async Task<IActionResult> CheckDangNhap(LoginRequest request)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-
-        //        ViewData["loginfalse"] = "";
-        //        if (HttpContext.Session.GetString("idkh") != null)
-        //        {
-        //            ViewData["loginfalse"] = "Bạn đã đăng nhập rồi";
-        //            return View("DangNhap");
-        //        }
-        //        else
-        //        {
-        //            var lstKH = await _services.GetAll<KhachHang>(StrConnection.StrConnection.api + "KhachHangs/Get-All");
-        //            if (string.IsNullOrEmpty(request.Email) || string.IsNullOrEmpty(request.MatKhau))
-        //            {
-        //                ViewData["loginfalse"] = "Tài khoản hoặc mật khẩu không được để trống";
-        //                return View("DangNhap");
-        //            }
-        //            else
-        //            {
-        //                var kh = lstKH.FirstOrDefault(p => p.Email == request.Email && p.MatKhau == request.MatKhau);
-        //                if (kh == null)
-        //                {
-        //                    ViewData["loginfalse"] = "Sai tài khoản hoặc mật khẩu";
-        //                    return View("DangNhap");
-        //                }
-        //                else
-        //                {
-        //                    HttpContext.Session.SetString("idkh", kh.Id.ToString());
-        //                    HttpContext.Session.SetString("ten", kh.Ten ?? "");
-
-        //                    var lstGH = await _services.GetAll<GioHang>(StrConnection.StrConnection.api + "GioHangs/Get-All");
-        //                    var gh = lstGH.FirstOrDefault(x => x.IdKH == kh.Id);
-        //                    if (gh != null)
-        //                        HttpContext.Session.SetString("idgh", gh.Id.ToString());
-        //                    return RedirectToAction("Index", "Home");
-        //                }
-        //            }
-        //        }
-        //    }
-        //    return View("DangNhap");
-        //}
+        
     }
 }
